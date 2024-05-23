51325d3e6763c9d899d32d9796d02f129f334035
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-05-23 14:10:49 -0500

    Parte1 (1.6: Unicafe, paso 1 / 1.7: Unicafe, paso 2)

--- Parte1/unicafe/src/App.jsx
+++ Parte1/unicafe/src/App.jsx
@@ -3,32 +3,55 @@ import { useState } from 'react'
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
-    //const [bad , setBad] = useState(0)
-  
+    const [bad, setBad] = useState(0)
+         
     const [totalgood, setTotalGood] = useState(0)
     const [totalneutral, setTotalNeutral] = useState(0)
-  
+    const [totalbad, setTotalBad] = useState(0)
+    const [allComments, setAllComments] = useState(0)
+    //const [average, setAverage] = useState(0)
+    
     const handleGoodClick = () => {
         const updatedGood = good + 1
-
         setGood(updatedGood)
         setTotalGood(updatedGood)
+
+        setAllComments(updatedGood + neutral + totalbad)
+        //setAverage(allComments / setAllComments)
     }
 
     const handleNeutralClick = () => {
         const updatedNeutral = neutral + 1
-
         setNeutral(updatedNeutral)
         setTotalNeutral(updatedNeutral)
+
+        setAllComments(totalgood + updatedNeutral + totalbad)
+        //setAverage((allComments) / setAllComments)
+    }
+
+    const handleBadClick = () => {
+        const updateBad = bad + 1
+        setBad(updateBad)
+        setTotalBad(updateBad)
+
+        setAllComments(totalgood + totalneutral + updateBad)
+        //setAverage((allComments) / setAllComments)
     }
   
     return (
         <div>
+            <h1>give feedback</h1>
             <button onClick={handleGoodClick}>Good</button>
             <button onClick={handleNeutralClick}>Neutral</button>
-         
-            <p>Good {totalgood}</p>
-            <p>Neutral {totalneutral}</p>
+            <button onClick={handleBadClick}>Bad</button>
+
+            <h2>statistics</h2>
+            <p>Good: {totalgood}</p>
+            <p>Neutral: {totalneutral}</p>
+            <p>Bad: {totalbad}</p>
+
+            <p>Total Comments: {allComments}</p>
+            
         </div>
     )
 }
