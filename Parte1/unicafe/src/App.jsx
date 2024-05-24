1fcbad851c9d60eb76a011c20fd08d34c21e329d
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-05-24 14:09:18 -0500

    Parte1 (1.6: Unicafe, paso 1 / 1.7: Unicafe, paso 2)

--- Parte1/unicafe/src/App.jsx
+++ Parte1/unicafe/src/App.jsx
@@ -5,39 +5,14 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
          
-    const [totalgood, setTotalGood] = useState(0)
-    const [totalneutral, setTotalNeutral] = useState(0)
-    const [totalbad, setTotalBad] = useState(0)
-    const [allComments, setAllComments] = useState(0)
-    //const [average, setAverage] = useState(0)
-    
-    const handleGoodClick = () => {
-        const updatedGood = good + 1
-        setGood(updatedGood)
-        setTotalGood(updatedGood)
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
 
-        setAllComments(updatedGood + neutral + totalbad)
-        //setAverage(allComments / setAllComments)
-    }
-
-    const handleNeutralClick = () => {
-        const updatedNeutral = neutral + 1
-        setNeutral(updatedNeutral)
-        setTotalNeutral(updatedNeutral)
-
-        setAllComments(totalgood + updatedNeutral + totalbad)
-        //setAverage((allComments) / setAllComments)
-    }
-
-    const handleBadClick = () => {
-        const updateBad = bad + 1
-        setBad(updateBad)
-        setTotalBad(updateBad)
-
-        setAllComments(totalgood + totalneutral + updateBad)
-        //setAverage((allComments) / setAllComments)
-    }
-  
+    const allComments = good + neutral + bad;
+    const average = allComments ? (good * 1 + neutral * 0 + bad * -1) / allComments : 0
+    const positivePercentage = allComments ? (good / allComments) * 100 : 0
+        
     return (
         <div>
             <h1>give feedback</h1>
@@ -46,12 +21,13 @@ const App = () => {
             <button onClick={handleBadClick}>Bad</button>
 
             <h2>statistics</h2>
-            <p>Good: {totalgood}</p>
-            <p>Neutral: {totalneutral}</p>
-            <p>Bad: {totalbad}</p>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
 
             <p>Total Comments: {allComments}</p>
-            
+            <p>Average: {average.toFixed(6)}</p>
+            <p>Positive Percentage: {positivePercentage.toFixed(6)}%</p>
         </div>
     )
 }
